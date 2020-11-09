74ad580d3013069b256617a7b9f115622854ecac
OzgurH <ozgur@hocaoglu.net>
2020-11-09 12:47:23 +0300

    VB.NET

--- ExportExcel.vb
+++ ExportExcel.vb
@@ -3,11 +3,7 @@ Imports System.IO
 Imports System.Web.Configuration
 Imports ClosedXML.Excel
 Module ExportExcel
-    Public Sub ExceleAktar(DosyaAdi As String, SQL As String)
-
-        'BU FONKSİYON PROJE ÇAPINDA VERİLEN SQL SORGUSUNDAN DÖNEN VERİLERİ
-        'EXCEL 2017+ FORMATINDA KULLANICININ İNDİRMESİNİ SAĞLAR.
-
+    Public Sub DoExport(FileName As String, SQL As String)
 
 
         Using con As New SqlConnection(ConnectionString)
@@ -18,13 +14,13 @@ Module ExportExcel
                     Using dt As New DataTable()
                         sda.Fill(dt)
                         Using wb As New XLWorkbook()
-                            wb.Worksheets.Add(dt, "TBFSis")
+        wb.Worksheets.Add(dt, "SheetName1")
 
                             HttpContext.Current.Response.Clear()
                             HttpContext.Current.Response.Buffer = True
                             HttpContext.Current.Response.Charset = ""
                             HttpContext.Current.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                            HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=" & DosyaAdi & ".xlsx")
+                            HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=" & FileName & ".xlsx")
                             Using MyMemoryStream As New MemoryStream()
                                 wb.SaveAs(MyMemoryStream)
                                 MyMemoryStream.WriteTo(HttpContext.Current.Response.OutputStream)
